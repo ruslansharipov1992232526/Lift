4bfd1e0c8275870ebcccea71b21474e13e9245c5
Ruslan Sharipov <ruslansharipov1992@yandex.ru>
2022-06-23 15:13:09 +0500

    удалил цикл вывода этажей

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,9 @@ import java.util.*;
 public class Main {
     public static Queue<Integer> floorTarget = new ArrayDeque<>();
     private static Scanner scanner = new Scanner(System.in);
-    private static void getFloor() {
-        int[] floor = new int[22];
-        for (int i = 1; i <= floor.length; i++) {
-            System.out.println(i + " этаж");
-        }
-    }
-
     public static void main(String[] args) {
         int previousFloor = -1;
         while (true) {
-            getFloor();
             System.out.println("Ожидаю ввода этажа: (для завершения введите 0)");
             int input = scanner.nextInt();
 
@@ -26,21 +18,19 @@ public class Main {
                 }
             }
 
-            while (!floorTarget.isEmpty()) {
+            System.out.println("Лифт проследовал по следующим этажам:");
 
-                System.out.println("Лифт проследовал по следующим этажам:");
-                int waitDoorsInSeconds = 10;
-                int waitMoveInSeconds = 5;
-                int totalSeconds = 0;
+            int waitDoorsInSeconds = 10;
+            int waitMoveInSeconds = 5;
+            int totalSeconds = 0;
 
-
+            while (input == 0) {
                 for (int startLift : floorTarget) {
-                    totalSeconds += waitDoorsInSeconds + waitMoveInSeconds;
                     System.out.print(" -> " + "Этаж " + startLift);
                 }
                 System.out.println();
                 System.out.println("Время затраченное лифтом на маршрут =: " + totalSeconds + " с.");
-                break;
+                return;
             }
         }
     }
