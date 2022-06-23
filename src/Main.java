12aed98c7874092292f16541f51fcf221290f7fb
Ruslan Sharipov <ruslansharipov1992@yandex.ru>
2022-06-23 16:27:03 +0500

    поменял методы и правильно расчитал время, но еще не рабоотает как надо

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import java.util.*;
 
 public class Main {
-    public static Queue<Integer> floorTarget = new ArrayDeque<>();
-    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        Queue<Integer> floorTarget = new ArrayDeque<>();
+        Scanner scanner = new Scanner(System.in);
         int previousFloor = -1;
+
         while (true) {
             System.out.println("Ожидаю ввода этажа: (для завершения введите 0)");
             int input = scanner.nextInt();
@@ -15,24 +16,36 @@ public class Main {
                 floorTarget.add(input);
                 if (input == 0) {
                     floorTarget.remove(0);
+                    break;
                 }
             }
+            previousFloor = input;
+        }
 
-            System.out.println("Лифт проследовал по следующим этажам:");
+        System.out.println("Лифт проследовал по следующим этажам:");
+        int waitDoorsInSeconds = 10;
+        int waitMoveInSeconds = 5;
+        int totalSeconds = 0;
+        previousFloor = -1;
 
-            int waitDoorsInSeconds = 10;
-            int waitMoveInSeconds = 5;
-            int totalSeconds = 0;
 
-            while (input == 0) {
-                for (int startLift : floorTarget) {
-                    System.out.print(" -> " + "Этаж " + startLift);
-                }
-                System.out.println();
-                System.out.println("Время затраченное лифтом на маршрут =: " + totalSeconds + " с.");
-                return;
+        while (!floorTarget.isEmpty()) {
+            System.out.printf("этаж %d ", floorTarget.peek());
+
+            if (floorTarget.peek() != 0) {
+                System.out.print("-> ");
             }
+
+            if (previousFloor != -1) {
+                totalSeconds += Math.abs(floorTarget.peek() - previousFloor) * waitMoveInSeconds;
+                totalSeconds += waitDoorsInSeconds;
+
+                previousFloor = floorTarget.peek();
+                floorTarget.poll();
+            }
+            System.out.println("Время затраченное лифтом на маршрут =: " + totalSeconds + " с.");
         }
     }
 }
+
 
